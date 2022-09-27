e9ec2accdecdd05e057f066da7ae429520de25d6
ikekiyo <ike.kyo0802@gmail.com>
2022-09-27 15:06:49 +0900

    add find Emplyee Api

---
+++ api/handler/employee_handler.go
@@ -0,0 +1,65 @@
+package handler
+
+import (
+	"context"
+	"math/rand"
+	"time"
+
+	api "evans.sample/gen"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+type EmployeeHandler struct {
+	api.EmployeeServiceServer
+}
+
+func NewEmployeeHandler() *EmployeeHandler {
+	return &EmployeeHandler{}
+}
+
+func employeeList() []*api.Employee {
+	return []*api.Employee{
+		{
+			Id:   1,
+			Name: "Yamada",
+			Age:  20,
+			Type: api.EmployeeType_FullTime,
+		},
+		{
+			Id:   2,
+			Name: "Suzuki",
+			Age:  25,
+			Type: api.EmployeeType_PartTime,
+		},
+		{
+			Id:   3,
+			Name: "Sasaki",
+			Age:  45,
+			Type: api.EmployeeType_FullTime,
+		},
+	}
+}
+
+func (h *EmployeeHandler) Employee(
+	ctx context.Context,
+	req *api.EmployeeRequest,
+) (*api.EmployeeResponse, error) {
+	employees := employeeList()
+	employee := api.Employee{}
+	for _, e := range employees {
+		if e.Id == req.Id {
+			employee = api.Employee{
+				Id:   e.Id,
+				Name: e.Name,
+				Age:  e.Age,
+				Type: e.Type,
+			}
+		}
+	}
+	return &api.EmployeeResponse{
+		Employee: &employee,
+	}, nil
+}
